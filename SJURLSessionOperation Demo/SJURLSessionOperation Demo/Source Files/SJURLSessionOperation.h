f4181331f98ccf6fa0a889cef68dd27f0d14b329
Soneé John <ysanna@me.com>
2016-02-17 10:20:51 -0400

    Update copyrights

--- SJURLSessionOperation Demo/SJURLSessionOperation Demo/Source Files/SJURLSessionOperation.h
+++ SJURLSessionOperation Demo/SJURLSessionOperation Demo/Source Files/SJURLSessionOperation.h
@@ -1,7 +1,7 @@
 /*
 The MIT License (MIT)
 
-Copyright (c) 2015 Soneé Delano John https://twitter.com/Sonee_John
+Copyright (c) 2015 - 2016 Soneé Delano John https://twitter.com/Sonee_John
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- SJURLSessionOperation Demo/SJURLSessionOperation Demo/main.m
+++ SJURLSessionOperation Demo/SJURLSessionOperation Demo/main.m
@@ -3,7 +3,7 @@
 //  SJURLSessionOperation Demo
 //
 //  Created by Soneé John on 2/16/16.
-//  Copyright © 2016 AlphaSoft. All rights reserved.
+//  Copyright © 2016 Soneé John. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
