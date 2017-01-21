267ecb5a54bf8dc4d3cc4473038a9a2a7971bf01
Soneé John <sonee@alphasoftware.co>
2017-01-21 19:15:18 -0400

    Add SJURLSessionKit

---
+++ SJURLSessionKit/SJURLSessionKit.h
@@ -0,0 +1,19 @@
+//
+//  SJURLSessionKit.h
+//  SJURLSessionKit
+//
+//  Created by Soneé John on 1/21/17.
+//  Copyright © 2017 AlphaSoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SJURLSessionKit.
+FOUNDATION_EXPORT double SJURLSessionKitVersionNumber;
+
+//! Project version string for SJURLSessionKit.
+FOUNDATION_EXPORT const unsigned char SJURLSessionKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SJURLSessionKit/PublicHeader.h>
+
+
---
+++ SJURLSessionKitTests/SJURLSessionKitTests.m
@@ -0,0 +1,39 @@
+//
+//  SJURLSessionKitTests.m
+//  SJURLSessionKitTests
+//
+//  Created by Soneé John on 1/21/17.
+//  Copyright © 2017 AlphaSoft. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SJURLSessionKitTests : XCTestCase
+
+@end
+
+@implementation SJURLSessionKitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
